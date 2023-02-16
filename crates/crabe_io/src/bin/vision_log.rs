79be58ce47c2e605bd6bfb8c4bc96ddb7c57a686
Etienne Schmitz <contact@etienne-schmitz.com>
2023-02-16 09:12:31 +0100

    Add some comments + remove string to str

--- crates/crabe_io/src/bin/vision_log.rs
+++ crates/crabe_io/src/bin/vision_log.rs
@@ -2,7 +2,7 @@ use crabe_io::network::multicast_udp_receiver::MulticastUDPReceiver;
 use crabe_protocol::protobuf::vision_packet::SslWrapperPacket;
 
 fn main() {
-    let mut vision = MulticastUDPReceiver::new("224.5.23.2".to_string(), 10020);
+    let mut vision = MulticastUDPReceiver::new("224.5.23.2", 10020);
     loop {
         if let Some(packet) = vision.receive::<SslWrapperPacket>() {
             dbg!(packet);
--- crates/crabe_io/src/network/multicast_udp_receiver.rs
+++ crates/crabe_io/src/network/multicast_udp_receiver.rs
@@ -3,25 +3,37 @@ use std::io::Cursor;
 use std::net::{Ipv4Addr, UdpSocket};
 use std::str::FromStr;
 
-/// TODO:
+/// A struct that handles a Multicast UDP Receiver.
 pub struct MulticastUDPReceiver {
+    /// The UDP socket that joins the multicast group.
     socket: UdpSocket,
+    /// A buffer that is used to receive data from the socket without allocating new heap memory.
     buffer: [u8; BUFFER_SIZE],
 }
 
-/// TODO:
 impl MulticastUDPReceiver {
-    /// Create a new Multicast UDP Receiver TODO
+    /// Creates a new Multicast UDP Receiver that joins the IPv4 multicast group.
+    ///
     /// # Arguments
     ///
-    /// * `ip`: The ip address of the Multicast UDP Receiver
-    /// * `port`: The port of of the Multicast UDP Receiver
+    /// * `ip`: The IP address of the Multicast UDP Receiver, in the form of a str slice.
+    /// * `port`: The port number of the Multicast UDP Receiver.
     ///
     /// # Returns
     ///
-    /// A Multicast UDP Receiver that join the multicast with the ipv4 address.
-    pub fn new(ip: String, port: u32) -> Self {
-        let ipv4 = Ipv4Addr::from_str(ip.as_str()).expect("TODO: Failed to parse vision server ip");
+    /// A new Multicast UDP Receiver that is ready to receive data.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use crabe_io::MulticastReceiver;
+    ///
+    /// let receiver = MulticastReceiver::new("224.5.23.2", 10020);
+    /// ```
+    ///
+    /// This example creates a new Multicast UDP Receiver that listens on IP address 224.5.23.2 and port 10020.
+    pub fn new(ip: &str, port: u32) -> Self {
+        let ipv4 = Ipv4Addr::from_str(ip).expect("TODO: Failed to parse vision server ip");
         let socket =
             UdpSocket::bind(format!("{}:{}", ip, port)).expect("Failed to bind the UDP Socket");
         socket
@@ -37,7 +49,16 @@ impl MulticastUDPReceiver {
         }
     }
 
-    /// TODO:
+    /// Creates a new Multicast UDP Receiver that joins the IPv4 multicast group.
+    ///
+    /// # Arguments
+    ///
+    /// * `ip`: The IP address of the Multicast UDP Receiver, in the form of a str slice.
+    /// * `port`: The port number of the Multicast UDP Receiver.
+    ///
+    /// # Returns
+    ///
+    /// A new Multicast UDP Receiver that is ready to receive data.
     pub fn receive<T: prost::Message + Default>(&mut self) -> Option<T> {
         if let Ok(p_size) = self.socket.recv(&mut self.buffer) {
             let packet = T::decode(Cursor::new(&self.buffer[0..p_size]))
