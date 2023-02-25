d393526ec2454e8151f77d6c15f1ce6ac11d8b4c
Etienne Schmitz <contact@etienne-schmitz.com>
2023-02-25 13:23:53 +0100

    continue to work on data structure

--- crates/crabe/src/world.rs
+++ crates/crabe/src/world.rs
@@ -2,29 +2,58 @@ use serde::{Deserialize, Serialize};
 
 pub const MAX_ROBOTS: usize = 16;
 
-#[derive(Debug, Default, Serialize, Deserialize, Copy, Clone)]
+#[derive(Debug)]
 pub struct Field {
     pub width: f32,
     pub length: f32,
-    pub goal_width: f32,
-    pub goal_depth: f32,
-    pub penalty_depth: f32,
-    pub penalty_width: f32,
-    pub center_radius: f32,
+}
+
+#[derive(Debug)]
+pub struct Goal {
+    pub width: f32,
+    pub depth: f32,
+}
+
+#[derive(Debug)]
+pub struct Penalty {
+    pub width: f32,
+    pub depth: f32,
+}
+
+#[derive(Debug)]
+pub struct Circle {
+    // center ?
+    pub radius: f32,
+}
+
+#[derive(Debug)]
+pub struct Geometry {
+    pub field : Field,
+    pub ally_goal: Goal,
+    pub opponent_goal: Goal,
+    pub ally_penalty: Penalty,
+    pub opponent_penalty: Penalty,
+    pub center: Circle,
 }
 
 pub struct AllyInfo;
 pub struct EnemyInfo;
 
 pub struct Robot<T> {
+    id: u32,
+    // pos : 2D
+    orientation: f32,
+    has_ball: bool,
     robot_info: T,
-
 }
 
-
+pub struct Ball {
+    // pos : 3D
+}
 
 pub struct World {
-    pub field: Option<Field>,
+    pub geometry: Geometry, // TODO : Add default value
     pub allies: [Option<Robot<AllyInfo>>; MAX_ROBOTS],
     pub enemies: [Option<Robot<EnemyInfo>>; MAX_ROBOTS],
+    pub ball: Ball,
 }
