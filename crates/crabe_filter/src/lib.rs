dfc75dc0247db8f87f6ae7e250952d6f17570fc2
Benjamin Chew <benjaminchew4@gmail.com>
2023-02-28 17:50:30 +0100

    use closure in filter

--- crates/crabe_filter/src/lib.rs
+++ crates/crabe_filter/src/lib.rs
@@ -3,11 +3,11 @@ use crabe_framework::component::FilterComponent;
 use crabe_framework::config::CommonConfig;
 use crabe_framework::data::receiver::InboundData;
 use crabe_framework::data::world::{AllyInfo, Ball, EnemyInfo, Robot, World};
+use crabe_protocol::protobuf::vision_packet::SslDetectionRobot;
 use nalgebra::{Point2, Point3};
 use ringbuffer::{ConstGenericRingBuffer, RingBuffer, RingBufferExt, RingBufferWrite};
 use std::collections::HashMap;
 use std::time::{Duration, Instant};
-use crabe_protocol::protobuf::vision_packet::SslDetectionRobot;
 
 #[derive(Args)]
 pub struct FilterConfig {}
@@ -33,7 +33,7 @@ struct CamField {}
 struct Tracked<T, U> {
     packets: ConstGenericRingBuffer<U, 50>,
     pub data: T,
-    pub last_update: Instant
+    pub last_update: Instant,
 }
 
 impl<T: Default, U> Default for Tracked<T, U> {
@@ -41,7 +41,7 @@ impl<T: Default, U> Default for Tracked<T, U> {
         Tracked {
             packets: ConstGenericRingBuffer::new(),
             last_update: Instant::now(),
-            data: Default::default()
+            data: Default::default(),
         }
     }
 }
@@ -60,11 +60,10 @@ impl<T, U> Tracked<T, U> {
         self.packets.push(packet);
     }
 
-    fn handle_packets(&mut self, packets: impl Iterator<Item=U>) {
+    fn handle_packets(&mut self, packets: impl Iterator<Item = U>) {
         self.packets.extend(packets);
     }
 }
-
 
 type TrackedRobot<T> = Tracked<Robot<T>, CamRobot>;
 type TrackedBall = Tracked<Ball, CamBall>;
@@ -97,38 +96,18 @@ impl FilterPipeline {
     }
 }
 
-fn map_camera_robots(
-    robots: impl Iterator<Item=SslDetectionRobot>,
-    camera_id: u32,
-    frame_number: u32,
-    time: f64,
-) -> impl Iterator<Item=CamRobot> {
-    robots.filter_map(move |r| {
-        if let Some(id) = r.robot_id {
-            Some(
-                CamRobot {
-                    id: id as usize,
-                    camera_id,
-                    position: Point2::new(r.x / 1000.0, r.y / 1000.0),
-                    orientation: r.orientation.unwrap_or(0.),
-                    time: Duration::from_secs_f64(time), // TODO: Check unit
-                    frame_number,
-                }
-            )
-        } else {
-            None
-        }
-    })
-}
-
 // TODO: Rename function?
-fn handle_camera_robots<T: Default>(robots: &mut TrackedRobotMap<T>, cam_robots: impl Iterator<Item=CamRobot>) {
+fn handle_camera_robots<T: Default>(
+    robots: &mut TrackedRobotMap<T>,
+    cam_robots: impl Iterator<Item = CamRobot>,
+) {
     cam_robots.for_each(|r| {
-        let robot = robots.entry(r.id as u32)
-            .or_insert_with(|| Tracked::new(Robot {
+        let robot = robots.entry(r.id as u32).or_insert_with(|| {
+            Tracked::new(Robot {
                 id: r.id as u32,
                 ..Default::default()
-            }));
+            })
+        });
 
         robot.handle_packet(r);
     })
@@ -140,11 +119,28 @@ impl FilterComponent for FilterPipeline {
             if let Some(mut detection) = packet.detection {
                 let camera_id = detection.camera_id;
                 let frame_number = detection.frame_number;
-                let time = detection.t_capture;
-                let yellow = map_camera_robots(detection.robots_yellow.drain(..), camera_id, frame_number, time);
-                let blue = map_camera_robots(detection.robots_blue.drain(..), camera_id, frame_number, time);
+                let time = Duration::try_from_secs_f64(detection.t_capture).unwrap(); // TODO: handle error & check if t_capture is in seconds
+                let map_cam_robots = |r| {
+                    if let Some(id) = r.robot_id {
+                        Some(CamRobot {
+                            id: id as usize,
+                            camera_id,
+                            position: Point2::new(r.x / 1000.0, r.y / 1000.0),
+                            orientation: r.orientation.unwrap_or(0.),
+                            time,
+                            frame_number,
+                        })
+                    } else {
+                        None
+                    }
+                };
+
+                let yellow = detection.robots_yellow.drain(..).filter_map(map_cam_robots);
+                let blue = detection.robots_blue.drain(..).filter_map(map_cam_robots);
+
                 let allies;
                 let enemies;
+
                 if self.yellow {
                     allies = yellow;
                     enemies = blue;
@@ -156,12 +152,12 @@ impl FilterComponent for FilterPipeline {
                 handle_camera_robots(&mut self.filter_data.allies, allies);
                 handle_camera_robots(&mut self.filter_data.enemies, enemies);
 
-                detection.balls.drain(..).for_each(|b| {
-                    self.filter_data.ball.packets.push(CamBall {
-                        camera_id,
-                        position: Point3::new(b.x, b.y, b.z.unwrap_or(0.0)),
-                    })
+                let ball_packets = detection.balls.drain(..).map(|x| CamBall {
+                    camera_id,
+                    position: Point3::new(b.x, b.y, b.z.unwrap_or(0.0)),
                 });
+
+                self.filter_data.ball.handle_packets(ball_packets);
             }
 
             if let Some(mut geometry) = packet.geometry {
