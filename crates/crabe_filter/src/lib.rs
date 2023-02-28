4ea9f0b562fc8d517de6999ec9c4818bb90354a6
Benjamin Chew <benjaminchew4@gmail.com>
2023-02-28 13:12:00 +0100

    handle camera packets

--- crates/crabe_filter/src/lib.rs
+++ crates/crabe_filter/src/lib.rs
@@ -6,7 +6,8 @@ use crabe_framework::data::world::{AllyInfo, Ball, EnemyInfo, Robot, World};
 use nalgebra::{Point2, Point3};
 use ringbuffer::{ConstGenericRingBuffer, RingBuffer, RingBufferExt, RingBufferWrite};
 use std::collections::HashMap;
-use std::time::Instant;
+use std::time::{Duration, Instant};
+use crabe_protocol::protobuf::vision_packet::SslDetectionRobot;
 
 #[derive(Args)]
 pub struct FilterConfig {}
@@ -23,20 +24,22 @@ struct CamRobot {
     pub camera_id: u32,
     pub position: Point2<f32>,
     pub orientation: f32,
-    pub time: Instant,
+    pub time: Duration,
     pub frame_number: u32,
 }
 
 struct CamField {}
 
 struct TrackedRobot<T> {
-    pub packets: ConstGenericRingBuffer<CamRobot, 50>, // TODO: Make circular vector
+    pub packets: ConstGenericRingBuffer<CamRobot, 50>,
+    // TODO: Make circular vector
     pub last_update: Instant,
     pub data: Robot<T>,
 }
 
 struct TrackedBall {
-    pub packets: ConstGenericRingBuffer<CamBall, 50>, // TODO: Make circular vector
+    pub packets: ConstGenericRingBuffer<CamBall, 50>,
+    // TODO: Make circular vector
     pub last_update: Instant,
     pub data: Ball,
 }
@@ -79,6 +82,48 @@ impl FilterPipeline {
     }
 }
 
+fn map_camera_packets(
+    robots: impl Iterator<Item=SslDetectionRobot>,
+    camera_id: u32,
+    frame_number: u32,
+    time: f64,
+) -> impl Iterator<Item=CamRobot> {
+    robots.filter_map(move |r| {
+        if let Some(id) = r.robot_id {
+            Some(
+                CamRobot {
+                    id: id as usize,
+                    camera_id,
+                    position: Point2::new(r.x / 1000.0, r.y / 1000.0),
+                    orientation: r.orientation.unwrap_or(0.),
+                    time: Duration::from_secs_f64(time), // TODO: Check unit
+                    frame_number,
+                }
+            )
+        } else {
+            None
+        }
+    })
+}
+
+// TODO: Rename function?
+fn handle_camera_packets<T: Default>(robots: &mut TrackedRobotMap<T>, cam_robots: impl Iterator<Item=CamRobot>) {
+    cam_robots.for_each(|r| {
+        let robot = robots.entry(r.id as u32)
+            .or_insert_with(|| TrackedRobot {
+                packets: ConstGenericRingBuffer::new(),
+                last_update: Instant::now(),
+                data: Robot {
+                    id: r.id as u32,
+                    ..Default::default()
+                },
+            });
+
+        robot.packets.push(r);
+    })
+}
+
+
 impl FilterComponent for FilterPipeline {
     fn step(&mut self, mut data: InboundData, world: &mut World) -> Option<World> {
         data.vision_packet.drain(..).for_each(|packet| {
@@ -86,77 +131,21 @@ impl FilterComponent for FilterPipeline {
                 let camera_id = detection.camera_id;
                 let frame_number = detection.frame_number;
                 let time = detection.t_capture;
+                let yellow = map_camera_packets(detection.robots_yellow.drain(..), camera_id, frame_number, time);
+                let blue = map_camera_packets(detection.robots_blue.drain(..), camera_id, frame_number, time);
+                let allies;
+                let enemies;
+                if self.yellow {
+                    allies = yellow;
+                    enemies = blue;
+                } else {
+                    allies = blue;
+                    enemies = yellow;
+                }
 
-                detection.robots_blue.drain(..).for_each(|r| {
-                    if let Some(id) = r.robot_id {
-                        if self.yellow {
-                            self.filter_data
-                                .enemies
-                                .entry(id)
-                                .or_insert_with(|| TrackedRobot {
-                                    packets: ConstGenericRingBuffer::new(),
-                                    last_update: Instant::now(),
-                                    data: Robot {
-                                        id,
-                                        position: Default::default(),
-                                        orientation: 0.0,
-                                        has_ball: false,
-                                        robot_info: EnemyInfo {},
-                                    },
-                                });
-                        } else {
-                            self.filter_data
-                                .allies
-                                .entry(id)
-                                .or_insert_with(|| TrackedRobot {
-                                    packets: ConstGenericRingBuffer::new(),
-                                    last_update: Instant::now(),
-                                    data: Robot {
-                                        id,
-                                        position: Default::default(),
-                                        orientation: 0.0,
-                                        has_ball: false,
-                                        robot_info: AllyInfo {},
-                                    },
-                                });
-                        }
-                    }
-                });
-                detection.robots_yellow.drain(..).for_each(|r| {
-                    if let Some(id) = r.robot_id {
-                        if self.yellow {
-                            self.filter_data
-                                .allies
-                                .entry(id)
-                                .or_insert_with(|| TrackedRobot {
-                                    packets: ConstGenericRingBuffer::new(),
-                                    last_update: Instant::now(),
-                                    data: Robot {
-                                        id,
-                                        position: Default::default(),
-                                        orientation: 0.0,
-                                        has_ball: false,
-                                        robot_info: AllyInfo {},
-                                    },
-                                });
-                        } else {
-                            self.filter_data
-                                .enemies
-                                .entry(id)
-                                .or_insert_with(|| TrackedRobot {
-                                    packets: ConstGenericRingBuffer::new(),
-                                    last_update: Instant::now(),
-                                    data: Robot {
-                                        id,
-                                        position: Default::default(),
-                                        orientation: 0.0,
-                                        has_ball: false,
-                                        robot_info: EnemyInfo {},
-                                    },
-                                });
-                        }
-                    }
-                });
+                handle_camera_packets(&mut self.filter_data.allies, allies);
+                handle_camera_packets(&mut self.filter_data.enemies, enemies);
+
                 detection.balls.drain(..).for_each(|b| {
                     self.filter_data.ball.packets.push(CamBall {
                         camera_id,
