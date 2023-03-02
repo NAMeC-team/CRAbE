36d6b645d37260a41e1ba5c9e92b60c4c8949d83
Benjamin Chew <benjaminchew4@gmail.com>
2023-03-02 13:42:13 +0100

    added vision prefilter

---
+++ crates/crabe_filter/src/pre_filters/vision.rs
@@ -0,0 +1,171 @@
+use crate::data::{CamBall, CamGeometry, CamRobot, FilterData, FrameInfo, TrackedBall, TrackedRobot, TrackedRobotMap};
+use crate::PreFilter;
+use chrono::{DateTime, LocalResult, TimeZone, Utc};
+use crabe_framework::data::receiver::InboundData;
+use crabe_framework::data::world::{AllyInfo, EnemyInfo, Robot, TeamColor};
+use crabe_protocol::protobuf::vision_packet::{SslDetectionBall, SslDetectionFrame, SslDetectionRobot, SslGeometryData};
+use log::error;
+use nalgebra::{Point2, Point3};
+use ringbuffer::RingBufferWrite;
+
+fn track_robots<T: Default>(
+    robots: &mut TrackedRobotMap<T>,
+    cam_robots: impl Iterator<Item=CamRobot>,
+) {
+    cam_robots.for_each(|r| {
+        let robot = robots.entry(r.id as u32).or_insert_with(|| TrackedRobot {
+            data: Robot {
+                id: r.id as u32,
+                ..Default::default()
+            },
+            ..Default::default()
+        });
+
+        robot.packets.push(r);
+    })
+}
+
+fn create_date_time(t_capture: f64) -> DateTime<Utc> {
+    match Utc.timestamp_opt((t_capture) as i64, 0) {
+        LocalResult::Single(dt) => dt,
+        LocalResult::None => {
+            let now_utc = Utc::now();
+            error!("Invalid timestamp, using current time: {}", now_utc);
+            now_utc
+        }
+        LocalResult::Ambiguous(dt_min, dt_max) => {
+            let dt_midpoint = dt_min + (dt_max - dt_min) / 2;
+            error!("Ambiguous timestamp resolved to midpoint: {}", dt_midpoint);
+            dt_midpoint
+        }
+    }
+}
+
+
+struct RobotDetectionInfo<'a> {
+    detected_blue: &'a [SslDetectionRobot],
+    detected_yellow: &'a [SslDetectionRobot],
+    tracked_allies: &'a mut TrackedRobotMap<AllyInfo>,
+    tracked_enemies: &'a mut TrackedRobotMap<EnemyInfo>,
+}
+
+fn detect_robots(detection: &mut RobotDetectionInfo, frame: &FrameInfo, team_color: &TeamColor) {
+    let map_packet = |r: &SslDetectionRobot| if let Some(id) = r.robot_id {
+        Some(CamRobot {
+            id,
+            frame_info: frame.clone(),
+            position: Point2::new(r.x, r.y),
+            orientation: r.orientation.unwrap_or(0.),
+            confidence: r.confidence,
+        })
+    } else {
+        None
+    };
+
+    let yellow = detection.detected_yellow
+        .iter()
+        .filter_map(map_packet);
+    let blue = detection.detected_blue
+        .iter()
+        .filter_map(map_packet);
+
+    let allies;
+    let enemies;
+
+    match team_color {
+        TeamColor::Yellow => {
+            allies = yellow;
+            enemies = blue;
+        }
+
+        _ => {
+            allies = blue;
+            enemies = yellow;
+        }
+    }
+
+    track_robots(detection.tracked_allies, allies);
+    track_robots(detection.tracked_enemies, enemies);
+}
+
+
+struct BallDetectionInfo<'a> {
+    detected: &'a [SslDetectionBall],
+    tracked: &'a mut TrackedBall,
+}
+
+fn detect_balls(detection: &mut BallDetectionInfo, frame: &FrameInfo) {
+    let ball_packets = detection.detected.iter().map(|b| CamBall {
+        frame_info: frame.clone(),
+        position: Point3::new(b.x, b.y, b.z.unwrap_or(0.0)),
+        confidence: b.confidence,
+    });
+
+    detection.tracked.packets.extend(ball_packets);
+}
+
+pub struct VisionFilter;
+
+impl VisionFilter {
+    pub fn new() -> VisionFilter {
+        VisionFilter
+    }
+
+    fn handle_detection(
+        detection: &SslDetectionFrame,
+        filter_data: &mut FilterData,
+        team_color: &TeamColor,
+    ) {
+        let frame_info = FrameInfo {
+            camera_id: detection.camera_id,
+            frame_number: detection.frame_number,
+            t_capture: create_date_time(detection.t_capture),
+        };
+
+        let mut robot_detection_info = RobotDetectionInfo {
+            detected_yellow: &detection.robots_yellow,
+            detected_blue: &detection.robots_blue,
+            tracked_allies: &mut filter_data.allies,
+            tracked_enemies: &mut filter_data.enemies,
+        };
+
+        detect_robots(&mut robot_detection_info, &frame_info, team_color);
+
+        let mut ball_detection_info = BallDetectionInfo {
+            detected: &detection.balls,
+            tracked: &mut filter_data.ball,
+        };
+
+        detect_balls(&mut ball_detection_info, &frame_info)
+    }
+
+    fn handle_geometry(geometry: &SslGeometryData, filter_data: &mut FilterData) {
+        let cam_geometry = CamGeometry {
+            field_length: geometry.field.field_length as f32,
+            field_width: geometry.field.field_width as f32,
+            goal_width: geometry.field.goal_width as f32,
+            goal_depth: geometry.field.goal_depth as f32,
+        };
+
+        filter_data.geometry = cam_geometry;
+    }
+}
+
+impl PreFilter for VisionFilter {
+    fn step(
+        &mut self,
+        inbound_data: &InboundData,
+        team_color: &TeamColor,
+        filter_data: &mut FilterData,
+    ) {
+        inbound_data.vision_packet.iter().for_each(|packet| {
+            if let Some(detection) = packet.detection.as_ref() {
+                Self::handle_detection(detection, filter_data, team_color);
+            }
+
+            if let Some(geometry) = packet.geometry.as_ref() {
+                Self::handle_geometry(geometry, filter_data);
+            }
+        });
+    }
+}
