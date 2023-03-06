c6532b9ff192d4c93b34f3ec1841c95a860009db
Etienne Schmitz <contact@etienne-schmitz.com>
2023-03-07 00:43:39 +0100

    Remove some another uom

--- crates/crabe_ai/src/pipeline.rs
+++ crates/crabe_ai/src/pipeline.rs
@@ -1,6 +1,4 @@
 use clap::Args;
-use uom::si::angular_velocity::radian_per_second;
-use uom::si::f32::AngularVelocity;
 use crabe_framework::component::{Component, DecisionComponent};
 use crabe_framework::config::CommonConfig;
 use crabe_framework::data::output::{Command, CommandMap, FeedbackMap};
@@ -30,17 +28,11 @@ impl DecisionComponent for DecisionPipeline {
     fn step(&mut self, data: &World) -> (CommandMap, ToolData) {
         let mut command_map = CommandMap::new();
         command_map.insert(0, Command {
-            angular_velocity: AngularVelocity::new::<radian_per_second>(1.0),
+            angular_velocity: 1.0,
             ..Default::default()
         });
 
 
         (command_map, ToolData {})
     }
-
-    // fn step(&mut self, _feedback: &mut FeedbackMap) -> InboundData {
-    //     let mut data = InboundData::default();
-    //     self.receivers.iter_mut().for_each(|x| x.fetch(&mut data));
-    //     data
-    // }
 }
