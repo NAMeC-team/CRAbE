10a8fdc5e23e0534bab8e17fd7bbf7230a539754
Benjamin Chew <benjaminchew4@gmail.com>
2023-03-08 19:09:09 +0100

    computer_order -> compute_order

--- crates/crabe_ai/src/action.rs
+++ crates/crabe_ai/src/action.rs
@@ -1,13 +1,15 @@
 pub mod move_to;
 pub mod sequencer;
 pub mod state;
-
+use enum_dispatch::enum_dispatch;
 use crate::action::move_to::MoveTo;
 use crate::action::sequencer::Sequencer;
 use crabe_framework::data::output::{Command, CommandMap};
 use state::State;
 use std::collections::HashMap;
+use std::ops::DerefMut;
 
+#[enum_dispatch(Actions)]
 pub trait Action {
     fn name(&self) -> String;
     fn state(&mut self) -> State;
@@ -16,21 +18,11 @@ pub trait Action {
     fn cancel(&mut self);
 }
 
-#[enum_dispatch(Action)]
+#[enum_dispatch]
 pub enum Actions {
-    MoveTo(MoveTo),
     Sequencer(Sequencer),
 }
 
-/*impl Actions {
-    fn computer_order(&mut self) -> Command {
-        match self {
-            Actions::MoveTo(b) => { b.compute_order()}
-            Actions::Sequencer(b) => { b.compute_order() }
-        }
-    }
-}
-*/
 #[derive(Default)]
 pub struct ActionWrapper {
     pub actions: HashMap<u16, Actions>,
@@ -38,6 +30,12 @@ pub struct ActionWrapper {
 
 impl ActionWrapper {
     pub fn push<T: Action>(&mut self, _action: T) {
+        let sequencer: Sequencer = Default::default();
+        let mut actions: Actions= sequencer.into();
+        actions.cancel();
+
+        let actions2 = &mut actions;
+        actions2.cancel();
         todo!()
     }
 
@@ -47,9 +45,7 @@ impl ActionWrapper {
 
     pub fn compute(&mut self) -> CommandMap {
         let mut command_map = CommandMap::default();
-        for action in self.actions.values_mut() {
-            command_map.insert(0, action.computer_order());
-        }
+        self.actions.values_mut().for_each(|action| {command_map.insert(0, action.compute_order());});
         command_map
     }
 }
