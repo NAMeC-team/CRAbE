268b1ef5233aaadc271debd15b40a8a63d13af06
Etienne Schmitz <contact@etienne-schmitz.com>
2023-03-11 08:55:16 +0100

    Improve comments

--- crates/crabe_framework/src/lib.rs
+++ crates/crabe_framework/src/lib.rs
@@ -3,8 +3,11 @@
 //! This crate provides shared utilities and settings for the CRAbE project.
 //!
 //! The `config` module contains settings that are common to multiple CRAbE
-//! crates. The `constant` module contains constants that are common to multiple
+//! crates.
+//! The `constant` module contains constants that are common to multiple
 //! CRAbE crates.
+//! The `component` module contains traits and structs that defines the component architecture
+//! used in the CRAbE project
 //!
 //! This crate is intended to be used as a dependency by other CRAbE crates, and
 //! should not be used on its own. Please refer to the individual modules for
@@ -18,13 +21,15 @@
 pub mod config;
 
 /// This module contains traits and structs that defines the component
-/// architecture used in the CRAbE project. Components are building blocks that
-/// perform a specific task or set of related tasks, and can be combined to form
-/// a pipeline for processing robot soccer data. The current components include
-/// an input component for receiving data, a filter component for processing and
-/// filtering the data, a decision component for making game decisions, a tools
-/// component for assisting in those decisions, a guard component for ensuring
-/// safe operation, and an output component for sending commands to the robots.
+/// architecture used in the CRAbE project.
+/// Components are building blocks that perform a specific task or set of related tasks,
+/// and can be combined to form a pipeline for processing robot soccer data.
+/// The current components include an input component for receiving data,
+/// a filter component for processing and filtering the data,
+/// a decision component for making game decisions,
+/// a tools component for assisting in those decisions,
+/// a guard component for ensuring safe operation,
+/// and an output component for sending commands to the robots.
 ///
 /// New components can be added by creating additional crates that implement the
 /// necessary traits. For example, while the current implementation of
