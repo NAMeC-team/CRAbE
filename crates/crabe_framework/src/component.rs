7fa1599ce7ab9f914aec87ab3cecf48fd63e0ba3
Etienne Schmitz <contact@etienne-schmitz.com>
2023-03-11 09:50:13 +0100

    Finish to document component.rs

--- crates/crabe_framework/src/component.rs
+++ crates/crabe_framework/src/component.rs
@@ -3,6 +3,9 @@ use crate::data::receiver::InboundData;
 use crate::data::tool::{ToolCommands, ToolData};
 use crate::data::world::World;
 
+/// The Component trait defines the methods shared for a component that is a part
+/// of the robot's AI pipeline.
+/// It includes a close method to be used when a component is no longer needed.
 pub trait Component: ComponentBoxed {
     fn close(self);
 }
@@ -43,24 +46,27 @@ pub trait InputComponent: Component {
 pub trait FilterComponent: Component {
     fn step(&mut self, data: InboundData, world: &mut World);
 }
+/// The `DecisionComponent` trait defines the methods required for a component that makes decisions
+/// for a SSL robot fleet based on the filtered input data.
+pub trait DecisionComponent: Component {
+    fn step(&mut self, data: &World) -> (CommandMap, ToolData);
+}
 
 /// The `ToolComponent` trait defines the methods required for a component that manages and
 /// manipulates additional tools used by the project's crates.
 /// These tools can include things like a joystick handler or sending and
 /// receiving data for tools, such as a viewer or a control center.
-pub trait DecisionComponent: Component {
-    fn step(&mut self, data: &World) -> (CommandMap, ToolData);
-}
-
 pub trait ToolComponent: Component {
     fn step(&mut self, world_data: &World, tools_data: &mut ToolData) -> ToolCommands;
 }
-
+/// The `GuardComponent` trait defines the methods required for a component that guards the robot
+/// from potentially dangerous or unwanted actions.
 pub trait GuardComponent: Component {
     fn step(&mut self, world: &World, command: &mut CommandMap, tools_commands: &mut ToolCommands);
 }
 
+/// The `OutputComponent` trait defines the methods required for a component that sends output
+/// commands and received feedback from the robot.
 pub trait OutputComponent: Component {
     fn step(&mut self, commands: CommandMap, tool_commands: ToolCommands) -> FeedbackMap;
 }
-// TODO: Document
