72ca1d6a13fa84eb6de9698e1266437423f431b4
Etienne Schmitz <contact@etienne-schmitz.com>
2023-03-11 10:09:29 +0100

    Document data.rs

--- crates/crabe_framework/src/data.rs
+++ crates/crabe_framework/src/data.rs
@@ -1,5 +1,13 @@
+/// The geometry module contains utility functions and structures related to geometry and
+/// coordinates of the SSL field.
 pub mod geometry;
+/// This module contains the input struct of the robot's control system.
 pub mod input;
+/// The output module contains the output struct of the robot's control system.
 pub mod output;
+/// The tool module contains the tool struct of the robot's control system,
+/// such as an annotation and a graph.
 pub mod tool;
+/// The world module contains the data structures for representing the state of the world
+/// in the control system. It includes information about robots, the ball, and the field.
 pub mod world;
