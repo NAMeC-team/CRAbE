8a6d1ff2076671a447d19ad0811b0d76ce7e480e
Etienne Schmitz <contact@etienne-schmitz.com>
2023-03-11 11:53:28 +0100

    Continue to document crabe_ai

--- crates/crabe_ai/src/manager.rs
+++ crates/crabe_ai/src/manager.rs
@@ -4,6 +4,12 @@ use crabe_framework::data::world::World;
 
 pub mod manual;
 
+/// The `Manager` trait defines a coach that handles the SSL game and gives each robot at least one strategy.
+/// A strategy is a behavior for one or multiple robots that gives one `Action` per robot. The `Manager`'s
+/// `step` function is called at each game step to update the strategies based on the current game state,
+/// and modify the `tools_data` and `action_wrapper` objects accordingly.
 pub trait Manager {
+    /// The step function is called by the DecisionPipeline on each iteration to compute the
+    /// appropriate actions to be taken by the robot based on the current game state.
     fn step(&mut self, data: &World, tools_data: &mut ToolData, action_wrapper: &mut ActionWrapper);
 }
--- crates/crabe_ai/src/manager/manual.rs
+++ crates/crabe_ai/src/manager/manual.rs
@@ -5,20 +5,28 @@ use crate::strategy::Strategy;
 use crabe_framework::data::tool::ToolData;
 use crabe_framework::data::world::World;
 
+/// The `Manual` struct represents a decision manager that executes strategies manually
+/// added to its list.
+/// It's used for testing individual strategies only and not meant to be used during an actual game.
+///
+/// To add a strategy, simply create a new instance of the desired strategy and add it to the
+/// `strategies` field in the `new()` method of the `Manual` struct.
 #[derive(Default)]
 pub struct Manual {
     strategies: Vec<Box<dyn Strategy>>,
 }
 
 impl Manual {
+    /// Creates a new `Manual` instance with the desired strategies to test.
     pub fn new() -> Self {
         Self {
-            strategies: vec![Box::<Square>::default()],
+            strategies: vec![Box::new(Square::new(0))],
         }
     }
 }
 
 impl Manager for Manual {
+    /// Executes the list of strategies on the given `World` data, `ToolData`, and `ActionWrapper`.
     fn step(
         &mut self,
         data: &World,
