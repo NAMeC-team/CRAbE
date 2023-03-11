c8a1fd185de1acc3b033fe33ca62ec75095340a0
Etienne Schmitz <contact@etienne-schmitz.com>
2023-03-11 12:06:01 +0100

    Document

--- crates/crabe_ai/src/action.rs
+++ crates/crabe_ai/src/action.rs
@@ -4,6 +4,7 @@ pub mod sequencer;
 pub mod state;
 
 use crate::action::move_to::MoveTo;
+use crate::action::order_raw::RawOrder;
 use crate::action::sequencer::Sequencer;
 use crabe_framework::data::output::{Command, CommandMap};
 use crabe_framework::data::tool::ToolData;
@@ -12,26 +13,42 @@ use enum_dispatch::enum_dispatch;
 use state::State;
 use std::collections::HashMap;
 
+/// The Action trait represents an action that can be performed by a robot, such as moving to a certain point.
 #[enum_dispatch(Actions)]
 pub trait Action {
+    /// Returns the name of the action.
     fn name(&self) -> String;
+    /// Returns the current state of the action.
     fn state(&mut self) -> State;
-
+    /// Computes the next command to be executed by the robot.
     fn compute_order(&mut self, id: u8, world: &World, tools: &mut ToolData) -> Command;
+    /// Cancel the action.
     fn cancel(&mut self);
 }
 
+/// The Actions enum is used to define the various actions that can be taken by a robot and implement the Action
 #[enum_dispatch]
 pub enum Actions {
     MoveTo(MoveTo),
+    RawOrder(RawOrder),
 }
 
+/// The `ActionWrapper` struct represents a wrapper for a sequence of actions to be executed for each robot.
+/// It provides methods to add an action to the sequence, clear the sequence for a specific robot, and compute
+/// the sequence of actions to be executed for each robot and return a `CommandMap` containing the commands
+/// to be sent to each robot.
 #[derive(Default)]
 pub struct ActionWrapper {
     pub actions: HashMap<u8, Sequencer>,
 }
 
 impl ActionWrapper {
+    /// Adds an action to the sequence of actions to be executed for a given robot.
+    ///
+    /// # Arguments
+    ///
+    /// * id: The id of the robot to which the action will be applied.
+    /// * action: The action to be added to the sequence.
     pub fn push<T: Action + Into<Actions>>(&mut self, id: u8, action: T) {
         if let Some(sequencer) = self.actions.get_mut(&id) {
             sequencer.push(action.into());
@@ -40,12 +57,24 @@ impl ActionWrapper {
         }
     }
 
+    /// Clears the sequence of actions to be executed for a given robot.
+    ///
+    /// # Arguments
+    ///
+    /// * `id`: The id of the robot whose sequence of actions will be cleared.
     pub fn clean(&mut self, id: u8) {
         if let Some(sequencer) = self.actions.get_mut(&id) {
             sequencer.clear();
         }
     }
 
+    /// Computes the sequence of actions to be executed for each robot and returns a
+    /// `CommandMap` containing the commands to be sent to each robot.
+    ///
+    /// # Arguments
+    ///
+    /// * `world`: The current state of the world.
+    /// * `tools`: A collection of external tools used by the action, such as a viewer.    .
     pub fn compute(&mut self, world: &World, tools: &mut ToolData) -> CommandMap {
         let mut command_map = CommandMap::default();
         self.actions.iter_mut().for_each(|(id, action)| {
--- crates/crabe_ai/src/strategy/square.rs
+++ crates/crabe_ai/src/strategy/square.rs
@@ -6,22 +6,41 @@ use crabe_framework::data::world::World;
 use nalgebra::Point2;
 use std::f64::consts::PI;
 
+/// The Square struct represents a strategy that commands a robot to move in a square shape
+/// in a counter-clockwise. It is used for testing purposes.
 #[derive(Default)]
 pub struct Square {
+    /// The id of the robot to move.
     id: u8,
 }
 
 impl Square {
+    /// Creates a new Square instance with the desired robot id.
     pub fn new(id: u8) -> Self {
         Self { id }
     }
 }
 
 impl Strategy for Square {
+    /// Executes the Square strategy.
+    ///
+    /// This strategy commands the robot with the specified ID to move in a square shape in a
+    /// counter-clockwise direction.
+    ///
+    /// # Arguments
+    ///
+    /// * data: The current state of the game world.
+    /// * tools_data: A collection of external tools used by the strategy, such as a viewer.    
+    /// * action_wrapper: An `ActionWrapper` instance used to issue actions to the robot.
+    ///
+    /// # Returns
+    ///
+    /// A boolean value indicating whether the strategy is finished or not.
+    #[allow(unused_variables)]
     fn step(
         &mut self,
-        _data: &World,
-        _tools_data: &mut ToolData,
+        data: &World,
+        tools_data: &mut ToolData,
         action_wrapper: &mut ActionWrapper,
     ) -> bool {
         action_wrapper.push(self.id, MoveTo::new(Point2::new(-1.0, 1.0), -PI / 4.0));
