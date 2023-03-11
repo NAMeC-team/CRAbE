83cccbc059f42f94079f1ebb39fa4ee901db22dd
Etienne Schmitz <contact@etienne-schmitz.com>
2023-03-11 15:22:55 +0100

    Document crabe_math at 100% !

--- crates/crabe_math/src/lib.rs
+++ crates/crabe_math/src/lib.rs
@@ -1,1 +1,9 @@
+//! A collection of math-related modules used in various parts of the CRAbE system.
+//!
+//! This crate includes the following modules:
+//!
+//! * `shape`: contains geometric primitives, such as `Line` and `Circle`, used to represent and manipulate shapes.
+
+/// The `shape` module contains geometric primitives, such as `Line` and `Circle`,
+/// which are used in various parts of the system to represent and manipulate shapes.
 pub mod shape;
