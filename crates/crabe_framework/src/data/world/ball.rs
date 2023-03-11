3568d7a8889a91f770a329fe4ec173a3e74e95f0
Etienne Schmitz <contact@etienne-schmitz.com>
2023-03-11 11:17:34 +0100

    Document ball and robot

--- crates/crabe_framework/src/data/world/ball.rs
+++ crates/crabe_framework/src/data/world/ball.rs
@@ -2,10 +2,15 @@ use chrono::{DateTime, Utc};
 use nalgebra::{Point3, Vector3};
 use serde::Serialize;
 
+/// The `Ball` struct represents the ball in the SSL game.
 #[derive(Serialize, Default, Clone, Debug)]
 pub struct Ball {
+    /// The position of the ball in 3D space.
     pub position: Point3<f64>,
+    /// The timestamp of when the data was captured.
     pub timestamp: DateTime<Utc>,
+    /// The velocity of the ball in 3D space.
     pub velocity: Vector3<f64>,
+    /// The acceleration of the ball in 3D space.
     pub acceleration: Vector3<f64>,
 }
--- crates/crabe_framework/src/data/world/robot.rs
+++ crates/crabe_framework/src/data/world/robot.rs
@@ -3,26 +3,38 @@ use nalgebra::{Point2, Vector2};
 use serde::Serialize;
 use std::collections::HashMap;
 
+/// The `AllyInfo` struct represents the information related to allies in the game.
 #[derive(Serialize, Clone, Default, Debug)]
 pub struct AllyInfo;
+
+/// The `EnemyInfo` struct represents the information related to enemies in the game.
 #[derive(Serialize, Clone, Default, Debug)]
 pub struct EnemyInfo;
 
+/// The `RobotVelocity` struct represents the velocity of a robot in the SSL.
 #[derive(Serialize, Default, Debug, Clone)]
 pub struct RobotVelocity {
+    /// The linear velocity of the robot in meters by second.
     pub linear: Vector2<f64>,
+    /// The angular velocity of the robot in radians by second.
     pub angular: f64,
 }
 
+/// The `RobotAcceleration` struct represents the acceleration of a robot in the SSL.
 #[derive(Serialize, Default, Debug, Clone)]
 pub struct RobotAcceleration {
+    /// The linear acceleration of the robot in meters by second.
     pub linear: Vector2<f64>,
+    /// The angular acceleration of the robot in radians by second.
     pub angular: f64,
 }
 
+/// The `Pose` struct represents the pose of a robot in the SSL, containing its position and orientation.
 #[derive(Serialize, Default, Debug, Clone)]
 pub struct Pose {
+    /// The orientation of the robot in radians.
     pub orientation: f64,
+    /// The position in 2D of the robot in meters.
     pub position: Point2<f64>,
 }
 
@@ -35,16 +47,25 @@ impl Pose {
     }
 }
 
+/// The `RobotMap` type is a hashmap that maps a robot ID to a Robot struct.
 pub type RobotMap<T> = HashMap<u32, Robot<T>>;
 
+/// The Robot struct represents a robot in the SSL game.
 #[derive(Serialize, Default, Debug)]
 pub struct Robot<T> {
+    /// The unique identifier of the robot.
     pub id: u32,
+    /// Whether or not the robot currently possesses the ball.
     pub has_ball: bool,
+    /// Additional information about the robot (can be `AllyInfo` or `EnemyInfo`)
     pub robot_info: T,
+    /// The current pose (position and orientation) of the robot.
     pub pose: Pose,
+    /// The current velocity of the robot.
     pub velocity: RobotVelocity,
+    /// The current acceleration of the robot.
     pub acceleration: RobotAcceleration,
+    /// The timestamp indicating when this information was last updated.
     pub timestamp: DateTime<Utc>,
 }
 
