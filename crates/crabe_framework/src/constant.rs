778db6615b13d2d1d372fd5a3b5a72eaffc1cf10
Etienne Schmitz <contact@etienne-schmitz.com>
2023-03-14 13:42:02 +0100

    Update MAX_ROBOT_ID to 15

--- crates/crabe_framework/src/constant.rs
+++ crates/crabe_framework/src/constant.rs
@@ -1,3 +1,3 @@
 /// The maximum ID number that can be assigned to a robot in the system.
 /// This value is determined by the rules of the Robocup SSL soccer league.
-pub const MAX_ID_ROBOTS: usize = 16;
+pub const MAX_ID_ROBOTS: usize = 15;
