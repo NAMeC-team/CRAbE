4a79befef84d5a7c93dca176b6e70f2a4601d9d3
Benjamin Chew <benjaminchew4@gmail.com>
2023-03-16 13:49:02 +0100

    added documentation for annotations

--- crates/crabe_framework/src/data/annotation.rs
+++ crates/crabe_framework/src/data/annotation.rs
@@ -17,22 +17,56 @@ pub enum Annotation {
 }
 
 impl AnnotationStore {
+    /// Add a circle annotation
+    ///
+    /// # Arguments
+    ///
+    /// * `id`: Annotation id
+    /// * `circle`: Circle to add
+    ///
+    /// returns: ()
     pub fn add_circle(&mut self, id: String, circle: Circle) {
         self.annotations.insert(id, Annotation::Circle(circle));
     }
 
+    /// Add a point annotation
+    ///
+    /// # Arguments
+    ///
+    /// * `id`: Annotation id
+    /// * `point`: Point to add
+    ///
+    /// returns: ()
     pub fn add_point(&mut self, id: String, point: Point2<f64>) {
         self.annotations.insert(id, Annotation::Point(point));
     }
 
+    /// Add a rectangle annotation
+    ///
+    /// # Arguments
+    ///
+    /// * `id`: Annotation id
+    /// * `rectangle`: Rectangle to add
+    ///
+    /// returns: ()
     pub fn add_rectangle(&mut self, id: String, rectangle: Rectangle) {
         self.annotations.insert(id, Annotation::Rectangle(rectangle));
     }
 
+    /// Clear annotations
+    ///
+    /// returns: ()
     pub fn clear(&mut self) {
         self.annotations.clear();
     }
 
+    /// Get an annotation
+    ///
+    /// # Arguments
+    ///
+    /// * `id`: Annotation id
+    ///
+    /// returns: Option<&Annotation>
     pub fn get(&self, id: &str) -> Option<&Annotation> {
         self.annotations.get(id)
     }
