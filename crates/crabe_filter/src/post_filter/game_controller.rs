3d960bbfaf0694009c66b60a22681c0754a7f0fc
Jossl123 <jossl1.gau@gmail.com>
2023-06-02 14:16:41 +0200

    remove dead code

--- crates/crabe_filter/src/post_filter/game_controller.rs
+++ crates/crabe_filter/src/post_filter/game_controller.rs
@@ -246,9 +246,6 @@ impl PostFilter for GameControllerPostFilter {
             self.previous_game_event = previous_game_event.clone();
             self.previous_event = previous_game_event.event.clone(); //todo: don't clone this, specify lifetime
         }
-
-        let referee_command = last_referee_packet.command();
-
         // if let Some(blue_team_on_positive_half) = last_referee_packet.blue_team_on_positive_half {
         //     if blue_team_on_positive_half {
         //        world.data.positive_half = TeamColor::Blue
@@ -256,108 +253,5 @@ impl PostFilter for GameControllerPostFilter {
         //         world.data.positive_half = TeamColor::Yellow
         //     }
         // };
-        //
-        // // from any state
-        // if let Command::Halt = referee_command {
-        //     world.data.state = GameState::Halted(HaltedState::Halt);
-        // }
-        //
-        // match referee_command {
-        //     Command::Halt => {
-        //         world.data.state = GameState::Halted(HaltedState::Halt);
-        //     }
-        //     Command::Stop => {
-        //         world.data.state = GameState::Stopped(StoppedState::Stop);
-        //     }
-        //     _ => {}
-        // }
-        //
-        // let mut kicker_team = None;
-        //
-        // match &world.data.state {
-        //     GameState::Halted(_) => {
-        //     }
-        //     GameState::Stopped(stopped_state) => {
-        //         match stopped_state {
-        //             StoppedState::Stop => {
-        //                 // TODO: prepare kickoffs :(
-        //                 match referee_command {
-        //                     Command::ForceStart => {
-        //                         world.data.state = GameState::Running(RunningState::Run);
-        //                     }
-        //                     Command::PrepareKickoffBlue => {
-        //                         kicker_team = Some(TeamColor::Blue);
-        //                         if world.team_color == TeamColor::Blue {
-        //                             world.data.state = GameState::Stopped(StoppedState::PrepareKickoff);
-        //                         } else {
-        //                             world.data.state = GameState::Stopped(StoppedState::Stop);
-        //                         }
-        //                     }
-        //                     Command::PrepareKickoffYellow => {
-        //                         kicker_team = Some(TeamColor::Yellow);
-        //                         if world.team_color == TeamColor::Yellow {
-        //                             world.data.state = GameState::Stopped(StoppedState::PrepareKickoff);
-        //                         } else {
-        //                             world.data.state = GameState::Stopped(StoppedState::Stop);
-        //                         }
-        //                     }
-        //                     Command::PreparePenaltyBlue => {}
-        //                     Command::PreparePenaltyYellow => {}
-        //                     Command::NormalStart => {
-        //                         world.data.state = GameState::Running(RunningState::KickOff(kicker_team.unwrap_or(TeamColor::Blue))); // FIX THIS auto color under too
-        //                     }
-        //                     _ => {}
-        //                 }
-        //             }
-        //             StoppedState::PrepareKickoff => {
-        //                 if let Command::NormalStart = referee_command {
-        //                     world.data.state = GameState::Running(RunningState::KickOff(kicker_team.unwrap_or(TeamColor::Blue)));
-        //                 }
-        //             }
-        //             StoppedState::PreparePenalty => {
-        //                 if let Command::NormalStart = referee_command {
-        //                     world.data.state = GameState::Running(RunningState::KickOff(kicker_team.unwrap_or(TeamColor::Blue)));
-        //                 }
-        //             }
-        //             StoppedState::BallPlacement => {
-        //                 // TODO: what the fuck ?
-        //                 // if let Command::Continue {
-        //                 //     world.data.state = GameState::Running(RunningState::KickOff);
-        //                 // }
-        //             }
-        //         }
-        //     }
-        //     GameState::Running(running_state) => {
-        //         match running_state {
-        //             RunningState::KickOff(_) => {
-        //                 if let Some(ball) = &world.ball {
-        //                     if ball.velocity.xy().norm() > 0.3 {
-        //                         world.data.state = GameState::Running(RunningState::Run);
-        //                     }
-        //                 }
-        //                 if let Some(chrono) = &self.chrono {
-        //                     if chrono.elapsed() > std::time::Duration::from_secs(10) {
-        //                         world.data.state = GameState::Running(RunningState::Run);
-        //                     }
-        //                 } else {
-        //                     // start chrono
-        //                     self.chrono = Some(Instant::now());
-        //                 }
-        //                 // if let Command::AfterXSecondsOrIfBallMoved {
-        //                 //     world.data.state = GameState::Running(RunningState::Run);
-        //                 // }
-        //             }
-        //             RunningState::FreeKick => {
-        //                 // if let Command::AfterXSecondsOrIfBallMoved {
-        //                 //     world.data.state = GameState::Running(RunningState::Run);
-        //                 // }
-        //             }
-        //             _ => {}
-        //         }
-        //     }
-        // }
-        //
-        //dbg!(referee_command);
-        //dbg!(&world.data.state);
     }
 }
