7ea280946a2b59f9d50597a95ea235b714958693
Bedeux <bedeux32@gmail.com>
2023-06-26 19:25:56 +0200

    Robots formation

--- crates/crabe_decision/src/strategy/testing.rs
+++ crates/crabe_decision/src/strategy/testing.rs
@@ -1,5 +1,9 @@
 /// The `square` module contains a strategy that commands a robot to move in a square shape
 /// in a counter-clockwise direction. It is used for testing purposes only and is not intended
 /// for use in a game.
-mod square;
-pub use self::square::Square;
+
+// mod square;
+// pub use self::square::Square;
+
+mod robots_formation;
+pub use self::robots_formation::RobotsFormation;
