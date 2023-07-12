416c056ba57707db29d7d12f5646eb9ff1460b35
Thomas Wanchai MENIER <thomas.menier@etu.u-bordeaux.fr>
2023-07-12 18:47:24 +0200

    refactor: remove code duplication

--- crates/crabe_decision/src/strategy/testing/test_vision_moveto.rs
+++ crates/crabe_decision/src/strategy/testing/test_vision_moveto.rs
@@ -31,63 +31,49 @@ impl TestVisionMoveTo {
             positive_half,
         }
     }
+
+
 }
 
 impl Strategy for TestVisionMoveTo {
     fn step(&mut self, world: &World, _: &mut ToolData, action_wrapper: &mut ActionWrapper) -> bool {
-        // WARNING : Not clearing the action_wrapper
+        // WARNING : Not clearing the action_wrapper leads to stuttering
         action_wrapper.clear();
         let sign = if self.positive_half { 1. } else { -1. };
-        let mut finished = world.allies_bot.len() > 0;
+        let mut y_target = 0.;
+        let mut next_status = TestVisionMoveToStatus::MovingForward;
         match self.status {
             TestVisionMoveToStatus::Placement => {
-                world.allies_bot.iter()
-                    .filter(|(ally_id, _)| self.ids.contains(ally_id))
-                    .for_each(|(ally_id, ally_info)| {
-                        let target = Point2::new((*ally_id as f64).div(2.) * sign, 0.);
-                        action_wrapper.push(*ally_id, MoveTo::new(
-                            target,
-                            0., 0., None, false, false),
-                        );
-                        finished = finished && distance(&ally_info.pose.position, &target) <= DIST_TARGET_REACHED;
-                    });
-
-                if finished {
-                    self.status = TestVisionMoveToStatus::MovingForward;
-                }
+                y_target = 0.;
+                next_status = TestVisionMoveToStatus::MovingForward;
             }
-
             TestVisionMoveToStatus::MovingForward => {
-                let mut change_status = true;
-                world.allies_bot.iter()
-                    .filter(|(ally_id, _)| self.ids.contains(ally_id))
-                    .for_each(|(ally_id, ally_info)| {
-                        let target_forward = Point2::new((*ally_id as f64).div(2.) * sign, 1.);
-                        action_wrapper.push(*ally_id, MoveTo::new(
-                            target_forward,
-                            0., 0., None, false, false),
-                        );
-                        change_status = distance(&target_forward, &ally_info.pose.position) <= DIST_TARGET_REACHED
-                });
-                self.status = if change_status { TestVisionMoveToStatus::MovingBackwards } else { TestVisionMoveToStatus::MovingForward };
+                y_target = 1.;
+                next_status = TestVisionMoveToStatus::MovingBackwards;
             }
-
             TestVisionMoveToStatus::MovingBackwards => {
-                let mut change_status = true;
-                world.allies_bot.iter()
-                    .filter(|(ally_id, _)| self.ids.contains(ally_id))
-                    .for_each(|(ally_id, ally_info)| {
-                    let target_backwards = Point2::new((*ally_id as f64).div(2.) * sign, -1.);
-                    action_wrapper.push(*ally_id, MoveTo::new(
-                        target_backwards,
-                        0., 0., None, false, false),
-                    );
-                    change_status = distance(&target_backwards, &ally_info.pose.position) <= DIST_TARGET_REACHED
-                });
-                self.status = if change_status { TestVisionMoveToStatus::MovingForward } else { TestVisionMoveToStatus::MovingBackwards };
+                y_target = -1.;
+                next_status = TestVisionMoveToStatus::MovingForward;
             }
         }
-        dbg!(&self.status);
+
+        // Move robots
+        let mut change_status = false;
+
+        world.allies_bot.iter()
+            .filter(|(ally_id, _)| self.ids.contains(ally_id))
+            .for_each(|(ally_id, ally_info)| {
+                let target = Point2::new((*ally_id as f64).div(2.) * sign, y_target);
+                action_wrapper.push(*ally_id, MoveTo::new(
+                    target,
+                    0., 0., None, false, false),
+                );
+                change_status = distance(&target, &ally_info.pose.position) <= DIST_TARGET_REACHED
+            });
+        if change_status {
+            self.status = next_status;
+        }
+
         false
     }
 
