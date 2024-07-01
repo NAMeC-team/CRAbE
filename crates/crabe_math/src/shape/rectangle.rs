503defa2a09e6e5d968610a543bf095aacc95eef
Clément Omen <clement.labbe33@gmail.com>
2024-07-01 17:56:21 +0200

    Implementing is inside function for rectangles

--- crates/crabe_math/src/shape/rectangle.rs
+++ crates/crabe_math/src/shape/rectangle.rs
@@ -13,4 +13,34 @@ pub struct Rectangle {
     pub height: f64,
     /// The position of the rectangle's top-left corner.
     pub position: Point2<f64>,
+
 }
+
+impl Rectangle {
+
+    pub fn new(width: f64, height: f64, position: Point2<f64>) -> Self {
+        Self { width, height, position }
+    }
+
+    /// Testing if a point is in the rectangle
+    /// ```
+    /// use nalgebra::Point2;
+    /// use crabe_math::shape::Rectangle;
+    /// let Rec = Rectangle::new(3.,3.,Point2::new(0., 0.));
+    /// assert_eq!(true, Rec.is_in(Point2::new(1., 1.)));
+    /// assert_eq!(true, Rec.is_in(Point2::new(0., 0.)));
+    /// assert_eq!(false, Rec.is_in(Point2::new(4., 4.)));
+    /// assert_eq!(false, Rec.is_in(Point2::new(-1., 2.)));
+    /// assert_eq!(false, Rec.is_in(Point2::new(2., -1.)));
+    /// ```
+
+    
+    
+    pub fn is_in(&self, point: Point2<f64>) -> bool {
+        let x = point.x - self.position.x;
+        let y = point.y - self.position.y;
+        x >= 0.0 && x <= self.width && y >= 0.0 && y <= self.height
+    }
+}
+
+
