d6ba36890c127c3b397e9f4292b42ea64548a79c
Caetano <84238339+dalestee@users.noreply.github.com>
2024-07-01 15:18:36 +0200

    Update robot.rs
    removed unwanted modifications

--- crates/crabe_framework/src/data/world/robot.rs
+++ crates/crabe_framework/src/data/world/robot.rs
@@ -6,10 +6,7 @@ use std::collections::HashMap;
 /// The `AllyInfo` struct represents the information related to allies in the game.
 #[derive(Serialize, Clone, Default, Debug)]
 #[serde(rename_all = "camelCase")]
-pub struct AllyInfo {
-    pub state: String,
-    pub message: String,
-}
+pub struct AllyInfo;
 
 /// The `EnemyInfo` struct represents the information related to enemies in the game.
 #[derive(Serialize, Clone, Default, Debug)]
