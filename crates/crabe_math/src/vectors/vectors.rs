3699f448bee1d518228f8974c3b5be21bf68f846
Jossl123 <jossl1.gau@gmail.com>
2024-07-01 10:42:48 +0200

    feat : tests

--- crates/crabe_math/src/vectors/vectors.rs
+++ crates/crabe_math/src/vectors/vectors.rs
@@ -29,11 +29,52 @@ pub fn angle_to_point(p1: Point2<f64>, p2: Point2<f64>) -> f64{
     let dir = p2.sub(p1);
     return (dir.y).atan2(dir.x);
 }
+
+/// Rotate a vector by a radian angle
+/// 
+/// # Arguments
+/// v - The vector to rotate
+/// angle - The angle in radians
+/// 
+/// # Returns
+/// The rotated vector
+/// 
+/// # Example
+/// ```
+/// use crabe_math::vectors::rotate_vector;
+/// use nalgebra::Vector2;
+/// 
+/// let v = Vector2::new(1.0, 0.0);
+/// let angle = std::f64::consts::FRAC_PI_2;
+/// let rotated_vector = rotate_vector(v, angle);
+/// // assert with delta
+/// assert!(rotated_vector.x - 0.0 < 0.00001);
+/// assert!(rotated_vector.y - 1.0 < 0.00001);
+/// ```
 pub fn rotate_vector(v: Vector2<f64>, angle: f64) -> Vector2<f64> {
     let rotation = Rotation2::new(angle);
     let rotated_vector = rotation * v;
     rotated_vector
 }
+
+/// Create a vector from an a radian angle
+/// 
+/// # Arguments
+/// angle - The angle in radians
+///     
+/// # Returns
+/// A (1, 0) vector rotated by the angle
+/// 
+/// # Example
+/// ```
+/// use crabe_math::vectors::vector_from_angle;
+/// use nalgebra::Vector2;
+/// 
+/// let angle = std::f64::consts::FRAC_PI_2;
+/// let vector = vector_from_angle(angle);
+/// // assert with delta
+/// assert!(vector.x - 0.0 < 0.00001);
+/// assert!(vector.y - 1.0 < 0.00001);
 pub fn vector_from_angle(angle: f64) -> Vector2<f64> {
     let vector = Vector2::new(1.0, 0.0); 
     let rotation = Rotation2::new(angle);
