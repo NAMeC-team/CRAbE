e9a2f47726a742070a9f8fc7d483efd318677e08
Josselin Gautier <71134166+Jossl123@users.noreply.github.com>
2024-07-03 16:58:27 +0200

    Update pipeline.rs

--- crates/crabe_decision/src/pipeline.rs
+++ crates/crabe_decision/src/pipeline.rs
@@ -29,7 +29,7 @@ impl DecisionPipeline {
     pub fn with_config(_decision_cfg: DecisionConfig, _common_cfg: &CommonConfig) -> Self {
         Self {
             action_wrapper: ActionWrapper::default(),
-            manager: Box::new(Manual::new()),
+            manager: Box::new(BigBro::new()),
         }
     }
 }
