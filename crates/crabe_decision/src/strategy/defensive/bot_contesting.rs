918ac58c40ac9d71f857c728a265384aff0b8ee7
Clément Omen <clement.labbe33@gmail.com>
2024-07-08 17:16:16 +0200

    Implementing constesting bot strategies

---
+++ crates/crabe_decision/src/strategy/defensive/bot_contesting.rs
@@ -0,0 +1,118 @@
+use crate::{
+    action::{move_to::MoveTo, ActionWrapper}, message::MessageData, strategy::Strategy
+};
+use crabe_framework::data::{
+    output::{Kick::StraightKick},
+    tool::ToolData,
+    world::World,
+};
+use nalgebra::Point2;
+use std::{time::Instant, vec};
+
+const ROBOT_RADIUS:f64 = 0.09;
+const BALL_RADIUS:f64 = 0.02;
+const DISTANCE_TO_BALL:f64 = ROBOT_RADIUS + BALL_RADIUS + 0.06;
+const ANGULAR_DIFFERENCE:f64 = 1.0;
+/// The BotContesting struct represents a strategy that commands a robot to move in a BotContesting shape
+/// in a counter-clockwise. It is used for testing purposes.
+pub struct BotContesting {
+    /// The id of the robot to move.
+    id: u8,
+    messages: Vec<MessageData>,
+    enemy_id: u8,
+    time: Instant,
+
+}
+
+fn look_at_target(robot: Point2<f64>, target: Point2<f64>) -> f64 {
+    let diff_x = target.x - robot.x;
+    let diff_y = target.y - robot.y;
+    diff_y.atan2(diff_x)
+}
+
+impl BotContesting {
+    /// Creates a new BotContesting instance with the desired robot id.
+    pub fn new(id: u8, enemy_id: u8) -> Self {
+        Self { 
+            id,
+            messages: vec![],
+            enemy_id,
+            time: Instant::now(),    
+        }
+    }
+}
+
+impl Strategy for BotContesting {
+    fn name(&self) -> &'static str {
+        "BotContesting"
+    }
+
+    fn get_messages(&self) -> &Vec<MessageData>  {
+        &self.messages
+    }
+    fn get_ids(&self) -> Vec<u8> {
+        vec![self.id]
+    }
+    fn put_ids(&mut self, ids: Vec<u8>) {
+        if ids.len() == 1{
+            self.id = ids[0];
+        }
+    }
+
+    #[allow(unused_variables)]
+    fn step(
+        &mut self,
+        world: &World,
+        tools_data: &mut ToolData,
+        action_wrapper: &mut ActionWrapper,
+        
+    ) -> bool {
+        action_wrapper.clear(self.id);
+        let ball: Point2<f64> = match &world.ball {
+            Some(b) => b,
+            None => {
+                eprintln!("Cannot find ball");
+                return false;
+            }
+        }.position_2d();
+
+        let robot = &match world.allies_bot.get(&self.id) {
+            Some(r) => r,
+            None => {
+                eprintln!("Cannot get robot");
+                return false;
+            }
+        }.pose;
+
+        let enemy = &match world.enemies_bot.get(&self.enemy_id) {
+            Some(r) => r,
+            None => {
+                eprintln!("Cannot get enemy");
+                return false;
+            }
+        }.pose;
+
+
+        let vector = enemy.position - ball;
+        let norm = vector.norm();
+        let coef = DISTANCE_TO_BALL/norm;
+        let target = enemy.position - Point2::new(vector.x, vector.y)*coef;
+        let angle = look_at_target(robot.position, enemy.position);
+
+        
+        
+        if self.time.elapsed().as_millis()%2 == 0{
+            action_wrapper.push(self.id,  MoveTo::new(Point2::new(target.x, target.y), angle+ANGULAR_DIFFERENCE, 0.0 , false , Some(StraightKick { power: 0.0 }), true ));
+        } else {
+             action_wrapper.push(self.id,  MoveTo::new(Point2::new(target.x, target.y), angle-ANGULAR_DIFFERENCE , 0.0 , false , Some(StraightKick { power: 0.0 }), true ));  
+        }
+        
+        
+        
+        return false;
+
+    }
+    
+    
+    
+}
