9a0c50921b01f1eeeb02bc59943cc35dcf75c124
Clément Omen <clement.labbe33@gmail.com>
2024-07-16 00:28:54 +0200

    Relsolving error

--- crates/crabe_decision/src/strategy/basics/move_away_from_point.rs
+++ crates/crabe_decision/src/strategy/basics/move_away_from_point.rs
@@ -4,6 +4,8 @@ use crabe_math::shape::Line;
 use crabe_math::vectors;
 use nalgebra::{distance, Point2};
 
+const INNACURACY: f64 = 0.2; // Using to avoid bugs when robot is already in the target position and turn around the point
+
 pub fn move_away(
     robot_pos: Point2<f64>,
     away_point: Point2<f64>,
@@ -14,12 +16,23 @@ pub fn move_away(
 
     let dist_robot_target = (robot_pos - away_point).norm();
     if dist_robot_target < distance{
-
+        if dist_robot_target == 0. {
+            return Some(
+                MoveTo::new( 
+                Point2::new(100.,100.),
+                0., 
+                0., 
+                false, 
+                None, 
+                true
+            ))
+        }
+        
         let target = away_point + (robot_pos - away_point).normalize();
 
         return Some(
-            MoveTo::new(away_point + (robot_pos - away_point).normalize() * distance
-            , orientation, 
+            MoveTo::new(target * (distance+INNACURACY),
+             orientation, 
             0., 
             false, 
             None, 
