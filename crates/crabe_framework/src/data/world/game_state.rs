c24a7fa3a2a395138a6a038bfdeaec3bf06d8a5e
Thomas Wanchai MENIER <wanchai.menier@gmail.com>
2024-12-26 19:27:12 +0100

    fix: remove old comments

--- crates/crabe_framework/src/data/world/game_state.rs
+++ crates/crabe_framework/src/data/world/game_state.rs
@@ -4,18 +4,6 @@ use serde::Serialize;
 /// Defines the possible game states of the match
 /// Some states are not associated with commands sent from the referee
 /// These states are marked with the [Non-official] tag
-///
-/// Some commands and events link to a similar state,
-/// for example some fouls lead to a Stop state.
-/// When states are regrouped together, they are marked
-/// by the [Collection] tag
-///
-/// The [Extension] tag means that there was more than one possibility for an
-/// event, and that we required to split it into multiple states
-/// For example, let's say the ball leaves the field by
-/// touching a goal line, and the ball was played on the side of Team A,
-/// the opponent being Team B. This event can lead to either a goal kick
-/// (if it is Team B's fault) or a corner kick (if it is Team A's fault)
 #[derive(Serialize, Copy, Clone, Debug, Eq, PartialEq)]
 #[serde(rename_all = "camelCase")]
 pub enum GameState {
@@ -33,14 +21,6 @@ pub enum HaltedState {
     Timeout(TeamColor),
 }
 
-/// A list of the most common Stop states that may occur
-/// during a game. Note that some states are behind a collection
-/// FoulStop, because they occur more rarely (such as a bot tipping over)
-/// The most common ones have been taken out of the collection, like the
-/// ball leaves field events. This is designed to explicitly define what
-/// the strategies should focus on, and what they can safely "ignore"
-/// to play a full game properly.
-///
 /// This distinction is purely subjective, but also comes from experience
 /// TODO: the layout of the `TeamColor` values is weird
 ///  sometimes, the TeamColor is the team responsible for the error
