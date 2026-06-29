5536782ba6cd0be1a7ed6f8c11bf3b16186ef254
Asolmele <ard.foussac@gmail.com>
2026-06-29 18:48:40 +0900

    feat: better handle of gamepad events and states

--- crates/crabe/src/gamepad_pipeline.rs
+++ crates/crabe/src/gamepad_pipeline.rs
@@ -1,11 +1,18 @@
-use gilrs::{Axis, Button, Event, GamepadId, Gilrs};
+use std::io::pipe;
+
+use crabe_io::tool;
+use gilrs::EventType::ButtonChanged;
+use gilrs::ev::Code;
+use gilrs::{Axis, Button, Event, Gamepad, GamepadId, Gilrs};
+use gilrs::EventType;
 use crabe_decision::pipeline::DecisionConfig;
 use crabe_framework::data::tool::ToolData;
 use crabe_framework::data::output::{Command, CommandMap, Kick};
 use crabe_framework::data::world::World;
 use crabe_framework::component::{Component, DecisionComponent};
 use crabe_framework::config::CommonConfig;
-use crabe_io::gamepad::GamepadRobotIdConfig;
+use crabe_io::gamepad::{self, GamepadRobotIdConfig};
+use log::info;
 
 pub struct GamepadPipeline {
     gilrs: Gilrs,
@@ -34,18 +41,60 @@ impl GamepadPipeline {
     }
 }
 
+
+fn handle_pressed(gamepad: Gamepad, button: Button, code: Code, command: &mut Command) {
+    match button {
+        Button::North => command.charge = !command.charge,
+        Button::LeftTrigger2 => command.kick = Some(Kick::ChipKick { power: 1.0 }),
+        Button::RightTrigger2 => command.kick = Some(Kick::StraightKick { power: 1.0 }),
+
+        _ => (),
+    }
+}
+
 impl DecisionComponent for GamepadPipeline {
     fn step(&mut self, world: &World) -> (CommandMap, ToolData) {
         // Examine new events
+        let mut gamepad_id = self.active_gamepad;
+        let tool_data = ToolData::default();
+        let mut command = Command::default();
+
+        // why store it then restore it like this ?
+        // well, i dont like too many arguments in my functions, this would clobber handle_pressed
+        command.charge = self.is_charging;
+
+        // events handle
         while let Some(Event { id, event, time }) = self.gilrs.next_event() {
-            // println!("{:?} New event from {}: {:?}", time, id, event);
-            self.active_gamepad = Some(id);
+            if gamepad_id.is_none() {
+                gamepad_id = Some(id);
+            }
+
+            let self_id = match gamepad_id {
+                Some(id) => id,
+                None => { self.active_gamepad = Some(id); id },
+            };
+
+            if self_id != id {
+                continue;
+            }
+
+            let gamepad = self.gilrs.gamepad(self_id);
+
+
+            match event {
+                EventType::ButtonPressed(button, code) => handle_pressed(gamepad, button, code, &mut command),
+                EventType::Connected =>  if self.active_gamepad == None { self.active_gamepad = Some(id) },
+                EventType::Disconnected => self.active_gamepad = None,
+
+                _ => info!("Unandled gamepad event !")
+            };
         }
 
-        // You can also use cached gamepad state
-        if let Some(gamepad) = self.active_gamepad.map(|id| self.gilrs.gamepad(id)) {
-            let mut command = Command::default();
+        self.active_gamepad = gamepad_id;
 
+        // state handle
+        info!("gamepad {:?}", self.active_gamepad);
+        if let Some(gamepad) = self.active_gamepad.map(|id| self.gilrs.gamepad(id)) {
             // Move Local Velocity
             if gamepad.value(Axis::LeftStickY).abs() > 0.2 {
                 command.forward_velocity = gamepad.value(Axis::LeftStickY);
@@ -70,30 +119,13 @@ impl DecisionComponent for GamepadPipeline {
             if gamepad.is_pressed(Button::LeftTrigger) {
                 command.dribbler = self.dribbler;
             }
-
-            if gamepad.is_pressed(Button::North) {
-                self.is_charging = !self.is_charging;
-            }
-
-            command.charge = self.is_charging;
-
-            // TODO : Handle power !
-            if gamepad.is_pressed(Button::RightTrigger2) {
-                command.kick = Some(Kick::StraightKick { power: 1.0 });
-            } else if gamepad.is_pressed(Button::LeftTrigger2) {
-                command.kick = Some(Kick::ChipKick { power: 1.0 });
-            }
-
-            let mut command_map = CommandMap::new();
-            command_map.insert(self.controlled_id, command);
-
-
-            (command_map, ToolData::default())
-        } else {
-            (CommandMap::default(), ToolData::default())
         }
-    }
 
+        self.is_charging = command.charge;
+        let mut command_map = CommandMap::new();
+        command_map.insert(self.controlled_id, command);
+        (command_map, tool_data)
+    }
 }
 
 impl Component for GamepadPipeline {
