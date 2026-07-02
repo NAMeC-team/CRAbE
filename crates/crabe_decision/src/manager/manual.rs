7e4a00a650925e699a03e9f8359c0cabfaf43cb5
Clément Labbé <clement.labbe33@gmail.com>
2026-07-02 19:21:50 +0200

    manual restoring

--- crates/crabe_decision/src/manager/manual.rs
+++ crates/crabe_decision/src/manager/manual.rs
@@ -1,13 +1,9 @@
-use std::collections::HashMap;
-
 use crate::action::ActionWrapper;
 use crate::manager::Manager;
-use crate::strategy::offensive::Attacker;
 use crate::strategy::testing::Square;
 use crate::strategy::Strategy;
-use crabe_framework::data::{tool::ToolData, world::RobotMap};
-use crabe_framework::data::world::{AllyInfo, World};
-use crate::exception::{Exception, card_exception::CardException};
+use crabe_framework::data::tool::ToolData;
+use crabe_framework::data::world::World;
 
 /// The `Manual` struct represents a decision manager that executes strategies manually
 /// added to its list.
@@ -18,23 +14,13 @@ use crate::exception::{Exception, card_exception::CardException};
 #[derive(Default)]
 pub struct Manual {
     strategies: Vec<Box<dyn Strategy>>,
-    exceptions: Vec<Box<dyn Exception>>,
-    benched: Vec<u8>,
 }
 
 impl Manual {
     /// Creates a new `Manual` instance with the desired strategies to test.
     pub fn new() -> Self {
-        let mut strategies: Vec<Box<dyn Strategy>> = vec![];
-        for i in 0..6 {
-            let strategy: Box<dyn Strategy> = Box::new(Attacker::new(i));
-            strategies.push(strategy);
-        }
-
         Self {
-            strategies: strategies,
-            exceptions: vec![Box::new(CardException::new())],
-            benched: vec![],
+            strategies: vec![Box::new(Square::new(0))],
         }
     }
 }
@@ -47,16 +33,7 @@ impl Manager for Manual {
         tools_data: &mut ToolData,
         action_wrapper: &mut ActionWrapper,
     ) {
-        self.exceptions.iter_mut().for_each(|x| x.step(world, tools_data, action_wrapper, &mut self.benched));
         self.strategies
-            .retain_mut(
-                |s|
-                if s.robots().iter().all(|r| !self.benched.contains(r))  {
-                    !s.step(world, tools_data, action_wrapper)
-                } else {
-                    true
-                }
-
-        );
+            .retain_mut(|s| !s.step(world, tools_data, action_wrapper));
     }
 }
