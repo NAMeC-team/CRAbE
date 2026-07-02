fe2a800e6caafb951f98256c388cdd0aeb1c7815
Clément Labbé <clement.labbe33@gmail.com>
2026-07-02 05:41:48 +0200

    wop

--- crates/crabe_decision/src/manager/state_based.rs
+++ crates/crabe_decision/src/manager/state_based.rs
@@ -1,5 +1,5 @@
 use std::vec;
-
+use clap::builder::styling::Color;
 use crate::action::state::State::Running;
 use crate::action::ActionWrapper;
 use crate::exception::Exception;
@@ -10,12 +10,15 @@ use crate::strategy::offensive::Attacker;
 use crate::strategy::testing::Square;
 use crate::strategy::Strategy;
 use crate::utils::{ATTACKER_ID, KEEPER_ID};
+use crate::strategy::rule_actions::{Halt,PrepareStart,PrepareKickOff,StrategyPenalty};
+use crate::strategy::formations::{MoveAwayFromBall, };
 use crabe_framework::data::tool::ToolData;
 use crabe_framework::data::world::game_state::{
     GameState, HaltedState, RunningState, StoppedState,
 };
 use crabe_framework::data::world::{self, World};
 use log::info;
+
 
 /// Manager handling game states, unnamed atm
 pub struct StateBasedManager {
@@ -33,47 +36,63 @@ impl StateBasedManager {
         }
     }
 
+    fn run(&mut self, world: &World, tools_data: &mut ToolData, action_wrapper: &mut ActionWrapper) {
+        let mut robots_left = 6 - self.benched.len();
+        let mut chosen_bots = vec![];
+
+        if !self.benched.contains(&KEEPER_ID) {
+            self.strategies.push(Box::new(GoalKeeper::new(KEEPER_ID)));
+            robots_left -= 1;
+            chosen_bots.push(KEEPER_ID);
+        }
+
+        if !self.benched.contains(&ATTACKER_ID) {
+            self.strategies.push(Box::new(Attacker::new(ATTACKER_ID)));
+            robots_left -= 1;
+            chosen_bots.push(ATTACKER_ID);
+        }
+
+        if (robots_left > 0) {
+            let mut wall_ids: Vec<u8> = vec![];
+            let mut id: u8 = 1;
+            while robots_left > 0 {
+                //id not in chosen bots or benched
+                if !(self.benched.contains(&id) || chosen_bots.contains(&id)){
+                    robots_left -= 1;
+                    wall_ids.push(id);
+                }
+
+                if id > 6 {
+                    info!("Maximum id achieved in running state");
+                    break;
+                }
+                id += 1;
+            }
+
+            self.strategies.push(Box::new(DefenseWall::new(wall_ids)));
+        }
+    }
+
     fn manage_running(&mut self, state: RunningState, world: &World, tools_data: &mut ToolData, action_wrapper: &mut ActionWrapper) {
         match state {
-            RunningState::KickOff(team_color) => info!("Kickoff todo"),
-            RunningState::Penalty(team_color) => info!("Penalty todo"),
+            RunningState::KickOff(team_color) => {info!("Kickoff todo");
+                if (team_color == world.team_color) {
+                    self.run(world, tools_data, action_wrapper);
+                } else {
+                    self.strategies.push(Box::new(PrepareKickOff::new(world.allies_bot.iter().map(|a| *a.0).collect(), team_color)));
+                }
+            },
+            RunningState::Penalty(team_color) => {info!("Penalty");
+                if (team_color == world.team_color){
+                    self.strategies.push(Box::new(StrategyPenalty::new(ATTACKER_ID, true)));
+                }else {
+                    self.strategies.push(Box::new(MoveAwayFromBall::new(world.allies_bot.iter().map(|a| *a.0).collect())));
+                }
+            },
             RunningState::FreeKick(team_color) => info!("FreeKick todo"),
             RunningState::Run =>  {
-                let mut robots_left = 6 - self.benched.len();
-                let mut chosen_bots = vec![];
-
-                if !self.benched.contains(&KEEPER_ID) {
-                    self.strategies.push(Box::new(GoalKeeper::new(KEEPER_ID)));
-                    robots_left -= 1;
-                    chosen_bots.push(KEEPER_ID);
-                }
-
-                if !self.benched.contains(&ATTACKER_ID) {
-                    self.strategies.push(Box::new(Attacker::new(ATTACKER_ID)));
-                    robots_left -= 1;
-                    chosen_bots.push(ATTACKER_ID);
-                }
-
-                if (robots_left > 0) {
-                    let mut wall_ids: Vec<u8> = vec![];
-                    let mut id: u8 = 1;
-                    while robots_left > 0 {
-                        //id not in chosen bots or benched
-                        if !(self.benched.contains(&id) || chosen_bots.contains(&id)){
-                            robots_left -= 1;
-                            wall_ids.push(id);
-                        }
-
-                        if id > 6 {
-                            info!("Maximum id achieved in running state");
-                            break;
-                        }
-                        id += 1;
-                    }
-
-                    self.strategies.push(Box::new(DefenseWall::new(wall_ids)));
-                }
-
+                info!("Run");
+                self.run(world, tools_data, action_wrapper);
             },
             RunningState::CornerKick(team_color) => info!("CornerKick todo"),
             RunningState::GoalKick(team_color) => info!("GoalKick todo"),
@@ -82,7 +101,9 @@ impl StateBasedManager {
 
     fn manage_halted(&mut self, state: HaltedState, world: &World, tool_data: &mut ToolData, action_wrapper: &mut ActionWrapper) {
         match state {
-            HaltedState::Halt => info!("Halt todo"),
+            
+            HaltedState::Halt => { info!("Halt");
+                                    self .strategies.push(Box::new(Halt::new(world.allies_bot.iter().map(|a| *a.0).collect())));},
             HaltedState::Timeout(team_color) => info!("Timeout todo"),
         }
     }
@@ -90,10 +111,20 @@ impl StateBasedManager {
 
     fn manage_stopped(&mut self, state: StoppedState, world: &World, tool_data: &mut ToolData, action_wrapper: &mut ActionWrapper) {
         match state {
-            StoppedState::PrepareKickoff(team_color) => info!("PrepareKickoff todo"),
-            StoppedState::PreparePenalty(team_color) => info!("PreparePenalty todo"),
-            StoppedState::BallPlacement(team_color) => info!("BallPlacement todo"),
-            StoppedState::Stop => info!("Stop todo"),
+
+            StoppedState::PrepareKickoff(team_color) => { info!("PrepareKickoff");
+                self.strategies.push(Box::new(PrepareKickOff::new(world.allies_bot.iter().map(|a| *a.0).collect(), team_color)));},
+            StoppedState::PreparePenalty(team_color) => {
+            if (team_color == world.team_color){
+                self.strategies.push(Box::new(StrategyPenalty::new(ATTACKER_ID, false)));
+            }else {
+                self.strategies.push(Box::new(MoveAwayFromBall::new(world.allies_bot.iter().map(|a| *a.0).collect())))
+            }
+            },
+            StoppedState::BallPlacement(team_color) => {info!("BallPlacement");
+                self .strategies.push(Box::new(Halt::new(world.allies_bot.iter().map(|a| *a.0).collect())));},
+            StoppedState::Stop => {info!("Stop todo");
+            self .strategies.push(Box::new(Halt::new(world.allies_bot.iter().map(|a| *a.0).collect())));},
             StoppedState::PrepareCornerKick(team_color) => info!("PrepareCornerKick todo"),
             StoppedState::PrepareGoalKick(team_color) => info!("PrepareGoalKick todo"),
         }
@@ -110,6 +141,7 @@ impl Manager for StateBasedManager {
     ) {
         self.strategies.clear();
         self.exceptions.iter_mut().for_each(|x| x.step(world, tools_data, action_wrapper, &mut self.benched));
+
         match world.data.ref_orders.state {
             GameState::Halted(state) => self.manage_halted(state, world, tools_data, action_wrapper),
             GameState::Stopped(state) => self.manage_stopped(state, world, tools_data, action_wrapper),
---
+++ crates/crabe_decision/src/strategy/rule_actions.rs
@@ -0,0 +1,11 @@
+mod halt;
+pub use self::halt::Halt;
+
+mod prepare_start;
+pub use self::prepare_start::PrepareStart;
+
+mod prepare_kick_off;
+pub use self::prepare_kick_off::PrepareKickOff;
+
+mod strategy_penalty;
+pub use self::strategy_penalty::StrategyPenalty;
