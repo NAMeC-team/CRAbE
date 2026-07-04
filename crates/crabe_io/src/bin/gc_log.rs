4e88d1beeb35ffd01cd195a4bef31f076e80afc9
Asolmele <ard.foussac@gmail.com>
2026-07-04 16:10:21 +0900

    chore: multicast config

--- crates/crabe_io/src/bin/gc_log.rs
+++ crates/crabe_io/src/bin/gc_log.rs
@@ -3,7 +3,7 @@ use crabe_protocol::protobuf::game_controller_packet::Referee;
 use std::net::Ipv4Addr;
 
 fn main() {
-    let mut vision = MulticastUDPReceiver::new(Ipv4Addr::new(255, 255, 255, 255), 10003)
+    let mut vision = MulticastUDPReceiver::new(Ipv4Addr::new(224, 5, 23, 1), 10003)
         .expect("Error to create GameController UDP Receiver");
     loop {
         if let Some(packet) = vision.receive::<Referee>() {
